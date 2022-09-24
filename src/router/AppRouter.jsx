756c7ea6312f9eb75a6f13c940968784573d015e
ddessilvestri <silvestri.des@gmail.com>
2022-09-24 15:21:05 -0300

    logout and login ready + save last path ready

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,12 +2,32 @@ import { Routes, Route } from 'react-router-dom';
 
 import { LoginPage } from '../auth';
 import { HeroesRoutes } from '../heroes/routes/HeroesRoutes';
+import { PrivateRoute } from '../heroes/routes/PrivateRoute';
+import { PublicRoute } from '../heroes/routes/PublicRoute';
 export const AppRouter = () => {
     return (
         <>
-            <Routes>              
-                <Route path="login" element={<LoginPage/>} />
-                <Route path="/*" element={<HeroesRoutes/>} />
+            <Routes> 
+                <Route path= "login/*" element={  //todo lo que venga con la palabra login
+                    <PublicRoute>
+                        {/* <LoginPage/> */}
+
+                        <Routes>
+                            <Route path="/*" element={<LoginPage/>} />
+                        </Routes>
+                    </PublicRoute>
+
+                }/>
+
+                {/* <Route path="login" element={<LoginPage/>} /> */}
+
+                <Route path="*" element={ 
+                    <PrivateRoute> 
+                        <HeroesRoutes/>
+                    </PrivateRoute>            
+                } />
+
+                {/* <Route path="/*" element={<HeroesRoutes/>} /> */}
              </Routes>
         </>
     )
